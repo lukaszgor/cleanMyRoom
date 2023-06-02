225e7cee673a831509ca45b05733f9c106d8ac55
Lukaszgor <lukasz.gg13@gmail.com>
2023-06-02 22:49:17 +0200

    add method

--- cleanmyroom/src/pages/AddRoom.js
+++ cleanmyroom/src/pages/AddRoom.js
@@ -1,23 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Button from '@mui/material/Button';
+import moment from "moment";
+import supabase from "../supabaseClient" 
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 
 function AddRoom() {
     const [number, setNumber] = useState('');
     const [name, setName] = useState('');
-    const [status, setStatus] = useState('');
+    const [status, setStatus] = useState('brudne');
     const [description, setDescription] = useState('');
+    const [date,setDate]=useState('09.02.2023')
   
    
     const handleStatusChange = (event) => {
       setStatus(event.target.value);
     };
+    useEffect(()=>{
+        currentDateFunction();
+      },[])
+  //insert
+const insertRoom = async()=>{
+    const{data,error} =  await supabase
+    .from('rooms')
+    .insert([{number:number,name:name,status:status,name:name,description:description,actionDate:date}])
+    if(error){
+        console.log(error)
+    }if(data){
+       console.log(data)
+    }
+  }
   
+  //set current date rewards
+  const currentDateFunction =()=>{
+    let currentDate=moment().format();   ; 
+    setDate(currentDate)
+  }
   
     const handleSubmit = (event) => {
       event.preventDefault();
@@ -25,8 +49,26 @@ function AddRoom() {
       console.log('Name:', name);
       console.log('Status:', status);
       console.log('Description:', description);
-      // Możesz wykonać inne operacje, takie jak wysłanie danych do serwera itp.
+      
+      
+      insertRoom();
+      handleClickAlert()
     };
+
+    //alert configuration
+const [open,setOpen] =useState(null)
+
+const handleClickAlert = () => {
+  setOpen(true);
+};
+
+const handleCloseAlert = (event, reason) => {
+  if (reason === 'clickaway') {
+    return;
+  }
+  setOpen(false);
+};
+
 
 
     return (
@@ -76,9 +118,16 @@ function AddRoom() {
         />
       </div>
       <Button type="submit" variant="contained" color="primary">
-        Submit
+        Zapisz
       </Button>
     </form>
+
+    <Snackbar open={open}
+        autoHideDuration={2000}
+        onClose={handleCloseAlert}>
+      <Alert severity="success">Correct!</Alert>
+      </Snackbar>
+
       </div>
      
      
