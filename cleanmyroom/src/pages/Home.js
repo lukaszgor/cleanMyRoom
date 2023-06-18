c5d2161b4365ac7a5b48a09cdaed35a91b4c8125
Lukaszgor <lukasz.gg13@gmail.com>
2023-06-18 11:27:33 +0200

    fixed bug in home component

--- cleanmyroom/src/pages/Home.js
+++ cleanmyroom/src/pages/Home.js
@@ -1,83 +1,68 @@
-import { useNavigate } from "react-router-dom"
-import { useState,useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
 import supabase from "../supabaseClient" 
 import Button from '@mui/material/Button';
-//import { Box } from "@mui/system";
-import RoomsDashboard from "../components/RoomsDashboard"
-import WorkerHome from "../pages/WorkerHome"
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
-import RoomCardDashbord from '../components/RoomCardDashbord'
+import RoomCardDashbord from '../components/RoomCardDashbord';
+import WorkerHome from '../pages/WorkerHome';
 
 function Home() {
-  const navigate = useNavigate()//add to nav
-  const [userID,setUserID] =useState('')
-//Verify loginuser
-    let userIdFromLocalStorage;
-    useEffect(()=>{
-      fetchTypeUser(); //check type !=null
-   // get user id
-   userIdFromLocalStorage = localStorage.getItem('userIdFromLocalStorage');
-   if(userIdFromLocalStorage ===null){
-     navigate('/')
-   }else{
-setUserID(userIdFromLocalStorage);
-   }
-   },[])
+  const navigate = useNavigate();
+  const [userID, setUserID] = useState('');
+  const [isVerified, setIsVerified] = useState(null);
 
-   const SignOut = async () => {
-    localStorage.clear();
-      const {user,error}= await supabase.auth.signOut()
-      if(error){
-        console("Error with sigout")
-      }else{
-        navigate('/')
-      }
+  useEffect(() => {
+    const userIdFromLocalStorage = localStorage.getItem('userIdFromLocalStorage');
+    if (userIdFromLocalStorage === null) {
+      navigate('/');
+    } else {
+      setUserID(userIdFromLocalStorage);
+      fetchTypeUser(userIdFromLocalStorage);
     }
+  }, []);
 
-//Verifity User
-const [isVerified,setisVerified] =useState(null)
-const [isBlocked,setIsBlocked] =useState(null)
-const fetchTypeUser = async()=>{
-  const{data,error} =  await supabase
-  .from('profiles')
-  .select()
-  .eq('id',userIdFromLocalStorage)
-  .single()
-  if(error){
-      console.log(error)
-  }if(data){
-    setisVerified(data.type)
-    setIsBlocked(data.blocked)
-    if(data.blocked ===1){
-      SignOut()
+  const SignOut = async () => {
+    localStorage.clear();
+    const { user, error } = await supabase.auth.signOut();
+    if (error) {
+      console.log("Error with sign out");
+    } else {
+      navigate('/');
+    }
+  };
+
+  const fetchTypeUser = async (userId) => {
+    const { data, error } = await supabase
+      .from('profiles')
+      .select('type')
+      .eq('id', userId)
+      .single();
+
+    if (error) {
+      console.log(error);
+    } else if (data) {
+      setIsVerified(data.type);
     }
   }
-}
 
   return (
     <div>
-     
-      {/* {isVerified !== null && <div><RoomsDashboard></RoomsDashboard></div>} */}
-      {isVerified === 'admin'  && <div><RoomCardDashbord></RoomCardDashbord></div>}
-      {isVerified === 'worker'  && <div><WorkerHome></WorkerHome></div>}
-      {isVerified === null && <div>
-    <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-    <p> Zaczekaj na zatwierdzenie administratora!</p>
-    <Button  onClick={SignOut}>
-  Wyloguj się
-</Button>
-<p></p>
-      <CircularProgress />
-      Twoje ID:
-<p>{userID}</p>
-
-    </Box>
-      </div>}
-     
+      {isVerified === 'admin' && <RoomCardDashbord />}
+      {isVerified === 'worker' && <WorkerHome />}
+      {isVerified === null && (
+        <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <p> Zaczekaj na zatwierdzenie administratora!</p>
+          <Button onClick={SignOut}>
+            Wyloguj się
+          </Button>
+          <p></p>
+          <CircularProgress />
+          Twoje ID:
+          <p>{userID}</p>
+        </Box>
+      )}
     </div>
-   
-    
   );
 }
 
